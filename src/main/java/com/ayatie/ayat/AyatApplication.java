cced35c6b4d903e6d481151c17df444961a85ac9
abook <abookyy@gmail.com>
2025-06-23 18:45:43 +0300

    23/6/2025

---
+++ src/main/java/com/ayatie/ayat/AyatApplication.java
@@ -0,0 +1,13 @@
+package com.ayatie.ayat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayatie/ayat/AyatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayatie.ayat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
